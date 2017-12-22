647d5bea1e22050235ed3b3770b8e8df5d5d8a78
Allied Consultants <omer@alliedc.com>
2017-12-22 14:56:27 +0500

    Add files via upload

---
+++ HubspotMigrator/HubspotMigrator/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace HubspotMigrator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.api1 = new System.Windows.Forms.TextBox();
+            this.api2 = new System.Windows.Forms.TextBox();
+            this.btnSync = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // api1
+            // 
+            this.api1.Location = new System.Drawing.Point(181, 69);
+            this.api1.Name = "api1";
+            this.api1.Size = new System.Drawing.Size(471, 20);
+            this.api1.TabIndex = 0;
+            // 
+            // api2
+            // 
+            this.api2.Location = new System.Drawing.Point(181, 134);
+            this.api2.Name = "api2";
+            this.api2.Size = new System.Drawing.Size(471, 20);
+            this.api2.TabIndex = 1;
+            // 
+            // btnSync
+            // 
+            this.btnSync.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSync.ForeColor = System.Drawing.Color.SeaGreen;
+            this.btnSync.Location = new System.Drawing.Point(523, 199);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(129, 23);
+            this.btnSync.TabIndex = 2;
+            this.btnSync.Text = "Sync";
+            this.btnSync.UseVisualStyleBackColor = true;
+            this.btnSync.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Source Account API KEY";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Destination Account API KEY";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(181, 199);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(171, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(228, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 31);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Hubspot Migrator";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnStop.Location = new System.Drawing.Point(377, 199);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(129, 23);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 254);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSync);
+            this.Controls.Add(this.api2);
+            this.Controls.Add(this.api1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox api1;
+        private System.Windows.Forms.TextBox api2;
+        private System.Windows.Forms.Button btnSync;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ HubspotMigrator/HubspotMigrator/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Dynamic;
+
+
+namespace HubspotMigrator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           // SyncEngagements("ae0e07b5-3c05-4bfb-b98c-f27a6b03c625", "199810d1-d2bb-4bf2-99a7-dab7f26d0808");
+           
+        }
+       
+        public string HttpGet(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            try
+
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                return "error";              
+            }
+        }
+        public static string HttpPost(string Url, string Data)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(Url);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = Data;
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+            return httpResponse.ToString();
+        }
+
+        public void AddNoteToContact(int contactID, string note,string HashKey)
+        {
+            string url = string.Format("https://api.hubapi.com/engagements/v1/engagements?hapikey={0}", HashKey);
+
+            dynamic jsonRequest = new ExpandoObject();
+            jsonRequest.engagement = new ExpandoObject();
+            jsonRequest.associations = new ExpandoObject();
+            jsonRequest.engagement.active = true;
+            jsonRequest.engagement.type = "NOTE";
+            jsonRequest.associations.contactIds = new int[] { contactID };
+            jsonRequest.metadata = new ExpandoObject();
+            jsonRequest.metadata.body = note;
+
+            string jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(jsonRequest);
+            HttpPost(url,jsonString);
+
+        }
+        public void AddEngagementoContact(int contactID, dynamic metadata, string type, string HashKey)
+        {
+            string url = string.Format("https://api.hubapi.com/engagements/v1/engagements?hapikey={0}", HashKey);
+
+            dynamic jsonRequest = new ExpandoObject();
+            jsonRequest.engagement = new ExpandoObject();
+            jsonRequest.associations = new ExpandoObject();
+            jsonRequest.engagement.active = true;
+            jsonRequest.engagement.type = type;
+            jsonRequest.associations.contactIds = new int[] { contactID };
+            jsonRequest.metadata = metadata;       
+            string jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(jsonRequest);
+            HttpPost(url, jsonString);
+
+        }
+
+
+
+        public void SyncEngagements(string SourceHashKey,string DestinationHashKey)
+        {
+            var counter = 0;
+            string offset = "0";
+            bool hasmore = false;
+
+            //Fetch all source contacts
+            do
+            {
+                if (hasmore.ToString().ToLower() == "true")
+                    hasmore = true;
+                else
+                    hasmore = false;
+
+                string url = string.Format("https://api.hubapi.com/contacts/v1/lists/all/contacts/all?hapikey=" + SourceHashKey + "&count=100&vidOffset=" + offset);
+                string resp = HttpGet(url);
+                if(resp=="error")
+                {
+                    MessageBox.Show("Please enter Valid API Keys");
+                    btnStop.PerformClick();
+                }
+                else
+                {
+                    dynamic d = Newtonsoft.Json.JsonConvert.DeserializeObject(resp);
+                    var Contacts = d.contacts;
+                    hasmore = d["has-more"];
+                    offset = d["vid-offset"];
+
+
+                    foreach (var item in Contacts)
+                    {
+                        var vid = item["vid"].Value;
+                        string ContactEmail = Convert.ToString(item["identity-profiles"][0].identities[0].value);
+                        //check if this user exists in the destination;
+                        string ContactDestinationUrl = string.Format("https://api.hubapi.com/contacts/v1/contact/email/" + ContactEmail + "/profile?hapikey=" + DestinationHashKey);
+                        string destResp = HttpGet(ContactDestinationUrl);
+                        if (destResp != "error")
+                        {
+                            dynamic dest = Newtonsoft.Json.JsonConvert.DeserializeObject(destResp);
+                            var DestContactVid = dest["vid"].Value;
+                            // fetch all the enagements for the current contact.
+                            string EngagementsUrl = string.Format(" https://api.hubapi.com/engagements/v1/engagements/associated/CONTACT/" + vid + "/?hapikey=" + SourceHashKey);
+
+                            string engResp = HttpGet(EngagementsUrl);
+                            dynamic e = Newtonsoft.Json.JsonConvert.DeserializeObject(engResp);
+                            var engagements = e.results;
+                            if (engagements.Count > 0)
+                            {
+                                //engagements loop
+                                foreach (var eng in engagements)
+                                {
+                                    string EngType = eng["engagement"].type;
+
+                                    //Check engagement type
+                                    if (EngType == "NOTE")
+                                    {
+                                        AddEngagementoContact(Convert.ToInt32(DestContactVid), eng["metadata"], "NOTE", DestinationHashKey);
+
+                                    }
+                                    else if (EngType == "EMAIL")
+                                    {
+                                        eng["metadata"].emailSendEventId = null;
+                                        eng["metadata"].trackerKey = null;
+                                        eng["metadata"].status = null;
+                                        eng["metadata"].sentVia = null;
+
+                                        AddEngagementoContact(Convert.ToInt32(DestContactVid), eng["metadata"], "EMAIL", DestinationHashKey);
+
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                    counter++;
+                }
+                
+            }
+            while (hasmore);
+            
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((api1.Text == "") || (api2.Text == ""))
+            {
+                MessageBox.Show("Please Enter API Keys");
+            }
+            else
+            {
+                progressBar1.Enabled = true;
+                progressBar1.Style = ProgressBarStyle.Marquee;
+                api1.Enabled = false;
+                api2.Enabled = false;
+                btnStop.Enabled = true;
+                btnSync.Enabled = false;
+                SyncEngagements(api1.Text, api2.Text);
+
+            }      
+            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnSync.Enabled = true;
+            progressBar1.Enabled = false;
+            progressBar1.Style = ProgressBarStyle.Blocks;
+            api1.Enabled = true;
+            api2.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+
+    }
+}
